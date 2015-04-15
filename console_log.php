b40bcb3a95fa15d7191fd5539606b00684950907
tajima <yuya.tajima@prime-strategy.co.jp>
2015-04-15 19:35:31 +0900

    expand tab.

--- console_log.php
+++ console_log.php
@@ -7,87 +7,87 @@ Author: Yuya Tajima
 */
 
 if ( ! function_exists('console_log') ) {
-	function console_log( $dump, $index = 1, $ajax = true, $echo = false ) {
+  function console_log( $dump, $index = 1, $ajax = true, $echo = false ) {
 
-		if ( ! isset( $dump ) ) {
-			$dump = NULL;
-		}
+    if ( ! isset( $dump ) ) {
+      $dump = NULL;
+    }
 
-		if( ! $ajax && ( defined( 'DOING_AJAX' ) && DOING_AJAX  ) ){
-			die();
-		}
+    if( ! $ajax && ( defined( 'DOING_AJAX' ) && DOING_AJAX  ) ){
+      die();
+    }
 
-		$debug_log = '';
+    $debug_log = '';
 
-		if ( defined('WP_CONTENT_DIR') ) {
+    if ( defined('WP_CONTENT_DIR') ) {
 
-			$debug_path_file = WP_CONTENT_DIR . '/console_log.php';
+      $debug_path_file = WP_CONTENT_DIR . '/console_log.php';
 
-			if ( ! file_exists( $debug_path_file ) ) {
-				touch( $debug_path_file );
-				chmod( $debug_path_file, 0666 );
-			}
+      if ( ! file_exists( $debug_path_file ) ) {
+        touch( $debug_path_file );
+        chmod( $debug_path_file, 0666 );
+      }
 
-			$debug_log = include( $debug_path_file );
-		}
+      $debug_log = include( $debug_path_file );
+    }
 
-		$file_size = filesize( $debug_log );
-		$file_size = (int) floor( $file_size / 1024 ) ;
+    $file_size = filesize( $debug_log );
+    $file_size = (int) floor( $file_size / 1024 ) ;
 
-		// if the log file size over 10MB, stop this flow immediately.
-		if ( $file_size > 10240 ) {
-			$fp = fopen( $debug_log, 'w+b' );
-			if ( is_resource( $fp ) ) {
-				flock( $fp, LOCK_EX );
-				fflush( $fp );
-				flock( $fp, LOCK_UN );
-				fclose( $fp );
-			}
-			return;
-		}
+    // if the log file size over 10MB, stop this flow immediately.
+    if ( $file_size > 10240 ) {
+      $fp = fopen( $debug_log, 'w+b' );
+      if ( is_resource( $fp ) ) {
+        flock( $fp, LOCK_EX );
+        fflush( $fp );
+        flock( $fp, LOCK_UN );
+        fclose( $fp );
+      }
+      return;
+    }
 
-		if( ! file_exists( $debug_log ) && ! is_writable( $debug_log ) ){
-			return;
-		}
+    if( ! file_exists( $debug_log ) && ! is_writable( $debug_log ) ){
+      return;
+    }
 
-		ob_start();
-		echo '*********************************************' . PHP_EOL;
-		_save_debug_log_backtrace($index);
-		var_dump( $dump );
-		echo '*********************************************' . PHP_EOL;
+    ob_start();
+    echo '*********************************************' . PHP_EOL;
+    _save_debug_log_backtrace($index);
+    var_dump( $dump );
+    echo '*********************************************' . PHP_EOL;
 
-		$out = ob_get_contents();
+    $out = ob_get_contents();
 
-		ob_end_clean();
+    ob_end_clean();
 
-		$save_debug_log = $out;
+    $save_debug_log = $out;
 
-		file_put_contents( $debug_log, $out, FILE_APPEND | LOCK_EX );
+    file_put_contents( $debug_log, $out, FILE_APPEND | LOCK_EX );
 
-		//if headers are not sending and $echo is true
-		//echo $dump
-		if( $echo && ! headers_sent() ){
-			echo nl2br( $save_debug_log );
-		}
-	}
+    //if headers are not sending and $echo is true
+    //echo $dump
+    if( $echo && ! headers_sent() ){
+      echo nl2br( $save_debug_log );
+    }
+  }
 
-	function _save_debug_log_backtrace( $index = 1, $LF = PHP_EOL  ) {
+  function _save_debug_log_backtrace( $index = 1, $LF = PHP_EOL  ) {
 
-		$debug_traces = debug_backtrace();
+    $debug_traces = debug_backtrace();
 
-		if ( function_exists('date_i18n') ) {
-			echo date_i18n('Y-m-d H:i:s') . $LF;
-		} else {
-			date_default_timezone_set( 'Asia/Tokyo' );
-			echo date('Y-m-d H:i:s') . $LF;
-		}
-		echo 'use memory(MB):' . round( memory_get_usage() / ( 1024 * 1024 ), 2 ) . 'MB' . $LF;
-		echo $LF;
-		echo 'called_file:' . $debug_traces[$index + 1]['file']. $LF;
-		echo 'called_file_line:' . $debug_traces[$index + 1]['line'] . $LF;
-		echo $LF;
-		echo 'current_file:' . $debug_traces[$index]['file']. $LF;
-		echo 'current_line:' . $debug_traces[$index]['line'] . $LF;
-		echo $LF;
-	}
+    if ( function_exists('date_i18n') ) {
+      echo date_i18n('Y-m-d H:i:s') . $LF;
+    } else {
+      date_default_timezone_set( 'Asia/Tokyo' );
+      echo date('Y-m-d H:i:s') . $LF;
+    }
+    echo 'use memory(MB):' . round( memory_get_usage() / ( 1024 * 1024 ), 2 ) . 'MB' . $LF;
+    echo $LF;
+    echo 'called_file:' . $debug_traces[$index + 1]['file']. $LF;
+    echo 'called_file_line:' . $debug_traces[$index + 1]['line'] . $LF;
+    echo $LF;
+    echo 'current_file:' . $debug_traces[$index]['file']. $LF;
+    echo 'current_line:' . $debug_traces[$index]['line'] . $LF;
+    echo $LF;
+  }
 }
