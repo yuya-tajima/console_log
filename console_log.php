ec58c83ba7d6845fe5af4f403c48d835bdb88e37
tajima <yuya.tajima@prime-strategy.co.jp>
2015-08-27 20:41:25 +0900

    Add comments and change arguments to an array.

--- console_log.php
+++ console_log.php
@@ -2,33 +2,57 @@
 /*
 Plugin Name: Console Log
 Description: store the var_dump results as a text file.
-Version: 0.7
+Version: 0.8
 Author: Yuya Tajima
 */
 
-if ( ! function_exists('console_log') ) {
-  function console_log( $dump, $any_time = false, $ajax = true, $index = 3,  $echo = false ) {
-    global $wp_did_header;
+/**
+ * This is a debugging tool. Store the var_dump results as a text file.
+ *
+ * @type mixed $dump The data you want to dump.
+ * @param array $args {
+ * available arguments.
+ *
+ * @type bool $any_time Whether to run this method anytime. default true.
+ *                      If false, run when $_GET['debug'] variable is setting.
+ * @type bool $wp_ajax Whether to run when WordPress Ajax is running. default true.
+ * @type int $index the number of index should be tarced. default 3.
+ * @type bool $echo Whether to output the $dump. default false.
+ * @type bool $extra Whether to show more information. default false.
+ *
+ * @author Yuya Tajima
+ * @link https://github.com/yuya-tajima/console_log
+ */
 
-    if ( ! $any_time ) {
-      if ( ! defined( 'ABSPATH' ) || ( isset( $wp_did_header ) && $wp_did_header )  || ( ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) && is_admin() )  ) {
-        if ( ! isset( $_GET['debug'] ) ) {
-          return;
-        }
-      }
-    }
+if ( ! function_exists( 'console_log' ) ) {
+  function console_log( $dump, array $args = array() ) {
 
     if ( ! isset( $dump ) ) {
       $dump = NULL;
     }
 
-    if( ! $ajax && ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ){
-      die(2);
+    $defaults = array(
+      'any_time' => false,
+      'wp_ajax'  => true,
+      'index'    => 3,
+      'echo'     => false,
+      'extra'    => false,
+    );
+
+    $args = array_merge( $defaults, $args );
+
+    if ( ! $args['any_time'] && empty( $_GET['debug'] ) ) {
+      return;
+    }
+
+    if( ! $args['wp_ajax'] && ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ){
+      return;
     }
 
     $debug_log = '';
 
-    if ( defined('WP_CONTENT_DIR') ) {
+
+    if ( defined( 'WP_CONTENT_DIR' ) ) {
 
       $debug_path_file = WP_CONTENT_DIR . '/console_log.php';
 
@@ -37,7 +61,7 @@ if ( ! function_exists('console_log') ) {
         chmod( $debug_path_file, 0666 );
         $str = <<<EOD
 <?php
-  return '/var/log/console.log';
+return '/var/log/console.log';
 EOD;
         file_put_contents( $debug_path_file, $str, LOCK_EX );
       }
@@ -45,17 +69,17 @@ EOD;
       $debug_log = include( $debug_path_file );
     }
 
-    if ( defined( 'CONSOLE_LOG_FILE' ) ) {
+    if ( defined( 'CONSOLE_LOG_FILE' ) && is_string( CONSOLE_LOG_FILE ) ) {
       $debug_log = CONSOLE_LOG_FILE;
     }
 
     if ( ! file_exists( $debug_log ) ) {
-      echo $debug_log . ' does not exist.' . PHP_EOL;
+      error_log( $debug_log . ' does not exist.' );
       return;
     }
 
     if ( ! is_writable( $debug_log ) ) {
-      echo $debug_log . ' is not writable. please change the file permission. or use another log file.' . PHP_EOL;
+      error_log( $debug_log . ' is not writable. please change the file permission. or use another log file.' );
       return;
     }
 
@@ -75,18 +99,18 @@ EOD;
     }
 
     if( ! file_exists( $debug_log ) ){
-        if ( touch( $debug_log ) ) {
-            chmod( $debug_log, 0666 );
-        } else {
-            return;
-        }
+      if ( touch( $debug_log ) ) {
+        chmod( $debug_log, 0666 );
+      } else {
+        return;
+      }
     }
 
     ob_start();
     echo '*********************************************' . PHP_EOL;
-    _console_log_backtrace($index);
-    if( defined( 'DOING_AJAX' ) && DOING_AJAX  ){
-      echo 'This is Ajax! by WordPress.' . PHP_EOL . PHP_EOL;
+    _console_log_backtrace( $args['index'], PHP_EOL, $args['extra'] );
+    if( defined( 'DOING_AJAX' ) && DOING_AJAX ){
+      echo 'Ajax is running! by WordPress.' . PHP_EOL . PHP_EOL;
       var_dump($_POST);
       echo PHP_EOL;
     }
@@ -100,14 +124,14 @@ EOD;
 
     file_put_contents( $debug_log, $out, FILE_APPEND | LOCK_EX );
 
-    //if headers have not already been sent and $echo is true
+    //if headers have not already been sent and $args['echo'] is true
     //echo $dump
-    if( $echo && ! headers_sent() ){
-      echo nl2br(  htmlspecialchars( $out , ENT_QUOTES ) );
+    if( $args['echo'] && ! headers_sent() ){
+      echo nl2br( htmlspecialchars( $out, ENT_QUOTES, 'UTF-8' ) );
     }
   }
 
-  function _console_log_backtrace( $index, $LF = PHP_EOL  ) {
+  function _console_log_backtrace( $index, $LF = PHP_EOL, $extra = false  ) {
 
     $debug_traces = debug_backtrace();
 
@@ -120,19 +144,18 @@ EOD;
     echo 'using memory(MB)  : ' . round( memory_get_usage() / ( 1024 * 1024 ), 2 ) . ' MB' . $LF;
     echo $LF;
 
-    var_dump( $_SERVER );
+    if ( $extra ) {
+      var_dump( $_SERVER );
+    }
 
     for ( $i = 0 ; ( $_index = $index - $i ) > 0 ; $i++ )  {
       echo isset( $debug_traces[$_index]['file'] ) ? 'file_name : ' . $debug_traces[$_index]['file']. $LF : '';
       echo isset( $debug_traces[$_index]['line'] ) ? 'file_line : ' . $debug_traces[$_index]['line'] . $LF : '';
       echo isset( $debug_traces[$_index]['class'] ) ? 'class_name : ' . $debug_traces[$_index]['class'] . $LF : '';
       echo isset( $debug_traces[$_index]['function'] ) ? 'func_name : ' . $debug_traces[$_index]['function'] . $LF : '';
-      if ( isset( $debug_traces[$_index]['args'] ) )  {
-        $args = $debug_traces[$_index]['args'];
-        if ( $args ){
-          $arg_string = trim( _getStringFromNotString( $args ) );
-          echo 'func_args : ' . $arg_string . $LF;
-        }
+      if ( isset( $debug_traces[$_index]['args'] ) && ( $args = $debug_traces[$_index]['args'] ) )  {
+        $arg_string = trim( _getStringFromNotString( $args ) );
+        echo 'func_args : ' . $arg_string . $LF;
       }
       echo $LF;
     }
@@ -149,9 +172,9 @@ EOD;
       $string .= ' (class) ' . get_class( $arg ) ;
     } elseif ( is_bool( $arg ) ) {
       if ( $arg ) {
-        $string .= ' true';
+        $string .= ' (bool)true';
       } else {
-        $string .= ' false';
+        $string .= ' (bool)false';
       }
     } else {
       if ( $arg === '' ) {
